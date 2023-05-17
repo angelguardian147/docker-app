bc5f81add2763aadf1c197461a77ce2e06c4b958
angelguardian147 <angelguardian147@gmail.com>
2023-05-17 16:09:35 -0500

    1.22

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var mysql = require('mysql2');
 
 var connection = mysql.createConnection({
   host: 'mysql-db',
-  port: 33060,
+  port: 3306,
   user: 'root',
   password: 'secret',
   database: 'enterprise',
